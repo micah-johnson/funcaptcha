318b0f37f457dc9a6d347e67e52d4af12b962525
BadAimWeeb <badaimweeb@protonmail.com>
2023-05-22 20:22:45 +0700

    upgrade to http2

--- lib/http.d.ts
+++ lib/http.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="node" />
 /// <reference types="node" />
-import { RequestOptions } from "undici/types/dispatcher";
-declare function req(url: string, options: RequestOptions, proxy?: string): Promise<{
+import * as http2 from "http2-wrapper";
+declare function req(url: string, options: http2.RequestOptions & {
+    body?: string;
+}, proxy?: string): Promise<{
     headers: import("http").IncomingHttpHeaders;
     body: Buffer;
 }>;
--- src/http.ts
+++ src/http.ts
@@ -1,27 +1,44 @@
-import { request, ProxyAgent } from "undici";
-// @ts-ignore
-import { RequestOptions } from "undici/types/dispatcher";
+import * as http2 from "http2-wrapper";
+import type Dispatcher from "undici/types/dispatcher";
+import { SocksClient } from "socks";
 
-async function req(url: string, options: RequestOptions, proxy?: string) {
-    let auth = undefined;
+const agentConnectionTable = new Map<string, http2.Agent>();
+
+async function req(url: string, options: http2.RequestOptions & { body?: string }, proxy?: string) {
+    let agent: http2.Agent | undefined = undefined;
     if (proxy) {
+        // proxy is expected to be a http proxy
         let proxyUrl = new URL(proxy);
-        if(proxyUrl.username && proxyUrl.password) {
-            auth = Buffer.from(proxyUrl.username + ":" + proxyUrl.password).toString("base64")
+        if (proxyUrl.protocol !== "http:") {
+            throw new Error("Only http proxy is supported");
         }
-    }
-    let dispatcher = proxy ? new ProxyAgent({
-        uri: proxy,
-        auth
-    }) : undefined;
 
-    let req = await request(url, {
-        ...options,
-        dispatcher,
+        agent = new http2.proxies.Http2OverHttp({
+            proxyOptions: {
+                url: proxyUrl
+            }
+        });
+    }
+
+    let req = http2.request(url, options);
+    if (options.body) req.write(options.body);
+    req.end();
+
+    let res = await new Promise<http2.IncomingMessage>((resolve, reject) => {
+        req.once("response", resolve);
+        req.once("error", reject);
     });
+
+    const body: Buffer[] = [];
+    res.on("data", (chunk) => body.push(chunk));
+    await new Promise<void>((resolve, reject) => {
+        res.once("end", resolve);
+        res.once("error", reject);
+    });
+
     return {
-        headers: req.headers,
-        body: Buffer.from(await req.body.arrayBuffer()),
+        headers: res.headers,
+        body: Buffer.concat(body),
     };
 }
 
