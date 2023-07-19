36f14eae4d74f3b2d66a1d3ae96cc420df886868
micah-johnson <57875046+micah-johnson@users.noreply.github.com>
2023-07-18 19:02:53 -0700

    Allow custom referer header for getToken req

--- src/api.ts
+++ src/api.ts
@@ -55,7 +55,7 @@ export async function getToken(
 
     if (options.site) {
         options.headers["Origin"] = options.surl
-        options.headers["Referer"] = `${options.surl}/v2/${options.pkey}/1.4.3/enforcement.${util.random()}.html`
+        options.headers["Referer"] = options.headers["referer"] || `${options.surl}/v2/${options.pkey}/1.4.3/enforcement.${util.random()}.html`
     }
     
     let ua = options.headers[Object.keys(options.headers).find(v => v.toLowerCase() == "user-agent")]
